9c4f6b3063aa485f3628e8efb12e43c8f16d301e
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-11-13 16:42:55 +0545

    added

---
+++ passwordhashing.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// Hashing a password
+async function hashPassword(plainPassword) {
+  return await bcrypt.hash(plainPassword, saltRounds);
+}
+
+// Verifying a password
+async function verifyPassword(plainPassword, hashedPassword) {
+  return await bcrypt.compare(plainPassword, hashedPassword);
+}
